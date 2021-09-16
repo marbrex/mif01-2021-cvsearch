ac2256cea892ee030fa0a3abbc5ecc9413df0d31
KASMAMYTOV ELDAR p1712650 <eldar.kasmamytov@etu.univ-lyon1.fr>
2021-09-16 18:41:11 +0200

    #3 Refactoring almost complete
    - Average and At Least One strategies are refactored

---
+++ src/main/java/fr/univ_lyon1/info/m1/cv_search/model/SearchStrategyAtLeastOne.java
@@ -0,0 +1,57 @@
+package fr.univ_lyon1.info.m1.cv_search.model;
+
+import fr.univ_lyon1.info.m1.cv_search.controllers.CvSearchController;
+import javafx.scene.control.Label;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchStrategyAtLeastOne implements SearchStrategy {
+
+    @Override
+    public List<Applicant> search() {
+        System.out.println("SearchStrategyAtLeastOne");
+
+        // building applicants' list,
+        // that is loading the data from the ".yaml" files into the ApplicantList class
+        ApplicantList listApplicants = new ApplicantListBuilder(new File(".")).build();
+
+        List<Applicant> res = new ArrayList<>();
+
+        System.out.println("Value = " + CvSearchController.getSelectedValue());
+
+        for (Applicant a : listApplicants) {
+            boolean selected = false;
+            System.out.println("Applicant = " + a.getName());
+
+            for (Label skill : CvSearchController.getSkillLabels()) {
+                String skillName = skill.getText();
+                System.out.println("Skill = " + skillName);
+
+                if (CvSearchController.isGreaterSignSelected()) {
+                    System.out.println("greater sign selected");
+                    if (a.getSkill(skillName) >= CvSearchController.getSelectedValue()) {
+                        System.out.println("is greater or equal than Value");
+                        selected = true;
+                        break;
+                    }
+                } else if (CvSearchController.isLessSignSelected()) {
+                    System.out.println("less sign selected");
+                    if (a.getSkill(skillName) <= CvSearchController.getSelectedValue()) {
+                        System.out.println("is less or equal than Value");
+                        selected = true;
+                        break;
+                    }
+                }
+            }
+
+            if (selected) {
+                System.out.println("Adding applicant");
+                res.add(a);
+            }
+        }
+
+        return res;
+    }
+}
--- src/main/java/fr/univ_lyon1/info/m1/cv_search/model/SearchStrategyAverage.java
+++ src/main/java/fr/univ_lyon1/info/m1/cv_search/model/SearchStrategyAverage.java
@@ -1,9 +1,49 @@
 package fr.univ_lyon1.info.m1.cv_search.model;
 
+import fr.univ_lyon1.info.m1.cv_search.controllers.CvSearchController;
+import javafx.scene.control.Label;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SearchStrategyAverage implements SearchStrategy {
 
     @Override
-    public void search() {
+    public List<Applicant> search() {
         System.out.println("SearchStrategyAverage");
+
+        // building applicants' list,
+        // that is loading the data from the ".yaml" files into the ApplicantList class
+        ApplicantList listApplicants = new ApplicantListBuilder(new File(".")).build();
+
+        List<Applicant> res = new ArrayList<>();
+
+        for (Applicant a : listApplicants) {
+            int skillLevelSum = 0;
+            int averageSkill;
+
+            for (Label skill : CvSearchController.getSkillLabels()) {
+                String skillName = skill.getText();
+                skillLevelSum += a.getSkill(skillName);
+            }
+            if (CvSearchController.getWantedSkillsCount() != 0) {
+                averageSkill = skillLevelSum / CvSearchController.getWantedSkillsCount();
+            } else {
+                averageSkill = 1;
+            }
+
+            if (CvSearchController.isGreaterSignSelected()) {
+                if (averageSkill >= CvSearchController.getSelectedValue()) {
+                    res.add(a);
+                }
+            } else if (CvSearchController.isLessSignSelected()) {
+                if (averageSkill <= CvSearchController.getSelectedValue()) {
+                    res.add(a);
+                }
+            }
+        }
+
+        return res;
     }
 }
