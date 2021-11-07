33b45ab4d14122a75ec6a2f6529b4c9abf0b3cb7
Melvyn <melvyn.bertolone--lopez-serrano@etu.univ-lyon1.fr>
2021-11-07 22:17:23 +0100

    Test SortApplicantsByCompaniesCountTest

---
+++ src/test/java/fr/univ_lyon1/info/m1/cv_search/model/sort/SortApplicantsByCompaniesCountTest.java
@@ -0,0 +1,32 @@
+package fr.univ_lyon1.info.m1.cv_search.model.sort;
+
+import fr.univ_lyon1.info.m1.cv_search.model.applicant.Applicant;
+import fr.univ_lyon1.info.m1.cv_search.model.applicant.ApplicantBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortApplicantsByCompaniesCountTest {
+    SortApplicantsByCompaniesCount c;
+
+    @BeforeEach
+    void setUp(){
+        c = new SortApplicantsByCompaniesCount();
+    }
+    @Test
+    @DisplayName("Simple comparaison should work")
+    void compare() {
+        // Given
+        ApplicantBuilder builder1 = new ApplicantBuilder("applicant1.yaml");
+        ApplicantBuilder builder2 = new ApplicantBuilder("applicant8.yaml");
+
+        // When
+        Applicant a = builder1.build();
+        Applicant b = builder2.build();
+
+        // Then
+        assertEquals(0, c.compare(a,b), "Basic comparaison between two applicants" );
+    }
+}
