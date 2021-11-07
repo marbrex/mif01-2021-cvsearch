e4d6f937af0bf72400d00d852d727f8b03513057
Melvyn <melvyn.bertolone--lopez-serrano@etu.univ-lyon1.fr>
2021-11-07 20:59:10 +0100

    Ajout du rapport sur le git

---
+++ src/test/java/fr/univ_lyon1/info/m1/cv_search/model/Applicant/ApplicantTest.java
@@ -0,0 +1,46 @@
+package fr.univ_lyon1.info.m1.cv_search.model.Applicant;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicantTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getSkill() {
+    }
+
+    @Test
+    void setSkill() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setSkillsAverage() {
+    }
+
+    @Test
+    void addExp() {
+    }
+
+    @Test
+    void setExpYears() {
+    }
+
+    @Test
+    void setCompaniesCount() {
+    }
+}
