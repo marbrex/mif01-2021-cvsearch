c32d41d2218578f91de2fbffcfa3554498cd2f0f
Melvyn <melvyn.bertolone--lopez-serrano@etu.univ-lyon1.fr>
2021-11-07 23:05:45 +0100

    sort basic tests

---
+++ src/test/java/fr/univ_lyon1/info/m1/cv_search/model/sort/SortApplicantsByNameTest.java
@@ -0,0 +1,30 @@
+package fr.univ_lyon1.info.m1.cv_search.model.sort;
+
+import fr.univ_lyon1.info.m1.cv_search.model.applicant.Applicant;
+import fr.univ_lyon1.info.m1.cv_search.model.applicant.ApplicantBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortApplicantsByNameTest {
+    SortApplicantsByName c;
+
+    @BeforeEach
+    void setUp(){
+        c = new SortApplicantsByName();
+    }
+    @Test
+    void compare() {
+        // Given
+        ApplicantBuilder builder1 = new ApplicantBuilder("applicant1.yaml");
+        ApplicantBuilder builder2 = new ApplicantBuilder("applicant8.yaml");
+
+        // When
+        Applicant a = builder1.build();
+        Applicant b = builder2.build();
+
+        // Then
+        assertEquals(-6, c.compare(a,b), "Basic comparaison between two applicants" );
+    }
+}
